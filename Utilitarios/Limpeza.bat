0454cef68c5dbc3c40ab9ffbbb34754f350ea5c9
Heyash <84398721+luanwolf@users.noreply.github.com>
2025-06-08 00:04:33 -0300

    Delete Utilitarios directory

--- Utilitarios/Limpeza.bat
+++
@@ -1,6 +0,0 @@
-@echo off 
-chcp 65001 
-setlocal enabledelayedexpansion 
-echo  [ 🧹 ] Limpando arquivos temporários... 
-del /q /f "C:\Users\Luan\AppData\Local\Temp\*" 
-del /q /f "C:\Windows\Temp\*" 
